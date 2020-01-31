3b8652db04fecd24e8a5f9a7883902949ba9b1f8
Caleb Brenner <calebwill91@gmail.com>
2020-01-31 16:15:32 -0600

    set up login routes in backend

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const Pupil = require('../../models/PupilUser');
 const Mentor = require('../../models/MentorUser');
@@ -18,6 +22,67 @@ router.get('/pupil', auth, async (req, res) => {
   }
 });
 
+// @route POST api/auth/pupil
+// @desc Authenticate user & get token
+// @access Public
+router.post(
+  '/pupil',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // See if user exists
+      let pupil = await Pupil.findOne({ email });
+
+      if (!pupil) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, pupil.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: pupil.id
+          // id that is generated for user when created
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+// *** ABOVE: PUPIL *** *** BELOW: MENTOR ***
+
 // @route GET api/auth/mentor
 // @desc Test route
 // @access Public
@@ -30,5 +95,64 @@ router.get('/mentor', auth, async (req, res) => {
     res.status(500).send('Server error');
   }
 });
+
+// @route POST api/auth/mentor
+// @desc Authenticate user & get token
+// @access Public
+router.post(
+  '/mentor',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // See if user exists
+      let mentor = await Mentor.findOne({ email });
+
+      if (!mentor) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, mentor.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: mentor.id
+          // id that is generated for user when created
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
